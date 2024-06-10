4f1bd590ca4a7a922cf6ccf184d368191c70ad9b
klimprakhon <klimprakhon@gmail.com>
2024-06-10 16:18:52 +0700

    add instructor in course search result

--- src/services/course-service.js
+++ src/services/course-service.js
@@ -3,7 +3,10 @@ const prisma = require("../models/prisma");
 const courseService = {};
 
 courseService.findCourseById = (courseId) => {
-  return prisma.course.findFirst({ where: { id: +courseId } });
+  return prisma.course.findFirst({
+    where: { id: +courseId },
+    include: { instructor: true },
+  });
 };
 
 module.exports = courseService;
